73d961dec8693c975eb446d8a7bcce66c2450bef
zhangdongdong <zhangdongdong@minivision.cn>
2020-05-19 14:17:07 +0800

    基本文件结构

---
+++ src/test/java/com/winter/datastructure/DataStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winter.datastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
